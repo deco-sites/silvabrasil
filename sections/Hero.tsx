99c89c550b02a55cb6d10abc62f6340d007efc73
igorbrands <igorcantelmo@gmail.com>
2024-05-28 23:35:38 -0300

    fix: button

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -20,71 +20,56 @@ export interface Props {
    */
   description?: string;
   image?: ImageWidget;
-  cta?: CTA[];
+  cta?: CTA;
 }
-
-
 
 export default function HeroFlats({
   title = "Click here to tweak this text however you want.",
-  description =
-    "Description text",image,
-  cta = [
-    { id: "change-me-1", href: "/", text: "Change me", outline: false },
-    { id: "change-me-2", href: "/", text: "Change me", outline: true },
-  ],
+  description = "Description text",
+  image,
+  cta,
 }: Props) {
   return (
-      <div class="flex flex-col items-center gap-8">
-        <div
-          class="relative flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 flex-col items-center justify-center text-center lg:py-36 gap-12 md:gap-20 items-center"
-        >
-          {image && (
-            <Image
-              width={640}
-              class="absolute inset-0 w-full h-full object-cover -z-10"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={image}
-              alt={image}
-              decoding="async"
-              loading="lazy"
-            />
-          )}
+    <div class="flex flex-col items-center gap-8">
+      <div class="relative flex w-full xl:mx-auto py-20 mx-5 md:mx-10 flex-col items-center justify-center text-center lg:py-36 gap-12 md:gap-20">
+        {image && (
+          <Image
+            width={640}
+            class="absolute inset-0 w-full h-full object-cover -z-10"
+            src={image}
+            alt={image}
+            decoding="async"
+            loading="lazy"
+          />
+        )}
+        <div class="text-green-900 container w-full lg:gap-12 gap-4 flex flex-col items-center justify-center">
           <div
-            class="mx-6 text-green-900 lg:mx-auto lg:w-full space-y-4 gap-4
-              flex flex-col items-center justify-center"
+            class="inline-block lg:text-[70px] text-4xl leading-none font-bold"
+            dangerouslySetInnerHTML={{
+              __html: title,
+            }}
           >
-            <div
-              class="inline-block lg:text-[70px] text-4xl leading-none font-bold"
-              dangerouslySetInnerHTML={{
-                __html: title,
-              }}
-            >
-            </div>
-            <div
-              class="text-base md:text-[32px] leading-[150%]"
-              dangerouslySetInnerHTML={{
-                __html: description,
-              }}
-            >
-            </div>
-            <div class="flex items-center justify-center gap-3">
-              {cta?.map((item) => (
-                <a
-                  key={item?.id}
-                  id={item?.id}
-                  href={item?.href}
-                  target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`font-normal btn btn-primary ${
-                    item.outline && "btn-outline"
-                  }`}
-                >
-                  {item?.text}
-                </a>
-              ))}
-            </div>
           </div>
+          <div
+            class="text-base md:text-[32px] leading-[150%]"
+            dangerouslySetInnerHTML={{
+              __html: description,
+            }}
+          >
+          </div>
+          <a
+            key={cta?.id}
+            id={cta?.id}
+            href={cta?.href}
+            target={cta?.href.includes("http") ? "_blank" : "_self"}
+            class="font-bold text-2xl bg-green-900 text-white py-6 px-[42px] flex justify-center items-center h-auto"
+          >
+            <span>
+              {cta?.text}
+            </span>
+          </a>
         </div>
       </div>
+    </div>
   );
 }
