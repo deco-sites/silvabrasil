c3bce98127451ded0f4b50621a6b6ec35d7ebf20
igorcantelmo <igor.cantelmo@calindra.com.br>
2024-06-21 13:38:13 -0300

    fonts adjustments

--- sections/TalkWithUs.tsx
+++ sections/TalkWithUs.tsx
@@ -78,6 +78,7 @@ export default function TalkWithUsSection(
         <div class="hidden lg:flex items-center justify-between">
           <svg
             xmlns="http://www.w3.org/2000/svg"
+			class="lg:hidden"
             width="190"
             height="75"
             viewBox="0 0 190 75"
