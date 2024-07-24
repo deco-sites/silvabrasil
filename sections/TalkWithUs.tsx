74ea12e6e888f45cd15bf1eb6f06d6e4054584b6
igorcantelmo <igor.cantelmo@calindra.com.br>
2024-07-24 19:39:48 -0300

    update

--- sections/TalkWithUs.tsx
+++ sections/TalkWithUs.tsx
@@ -66,7 +66,7 @@ export default function TalkWithUsSection({
       id="talk-with-us"
       class="container size-full flex flex-col lg:flex-row bg-green-900 lg:bg-opacity-90 lg:rounded-xl"
     >
-      <div class="w-full h-auto lg:w-2/3 lg:px-20 pt-9 lg:pt-0 lg:py-24 bg-green-900 lg:rounded-xl px-6 mb-9 lg:mb-0">
+      <div class="w-full h-auto lg:w-2/3 lg:px-28 pt-9 lg:py-[92px] bg-green-900 lg:rounded-xl px-6 mb-9 lg:mb-0">
         <div class="h-full justify-between flex flex-col items-start text-left">
           <div class="mb-6 lg:max-w-[407px]">
             <h2 class="text-4xl lg:text-5xl font-bold text-slate mb-2 font-serif">
@@ -82,14 +82,14 @@ export default function TalkWithUsSection({
           <a
             href={cta?.href}
             target={cta?.href.includes("http") ? "_blank" : "_self"}
-            class="inline-flex items-center justify-center font-bold text-base bg-green-500 text-white py-3 px-6 lg:px-0 rounded-lg transform transition duration-400 hover:scale-105"
+            class="inline-flex items-center justify-center font-bold text-base bg-green-500 text-white py-4 px-6 rounded-lg transform transition duration-400 hover:scale-105"
           >
             <Icon id="WhatsApp" width={14} height={16} class="mr-2" />
             {cta?.text}
           </a>
         </div>
       </div>
-      <div class="w-full h-auto lg:w-1/3 lg:rounded-xl px-6 lg:px-20 pb-12 lg:pb-0 lg:py-24">
+      <div class="w-full h-auto lg:w-1/3 lg:rounded-xl px-6 lg:px-[88px] pb-12  lg:py-24">
         {form && (
           <form onSubmit={handleSubmit} class="flex flex-col gap-6">
             <div class="w-full gap-2">
