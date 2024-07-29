737eca982fbb5e70f363f93f6dd975cc57ac5293
igorcantelmo <igor.cantelmo@calindra.com.br>
2024-07-29 09:50:10 -0300

    form send

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -5,7 +5,8 @@
 import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
 import * as $$$$$$$$$$$1 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$2 from "./apps/deco/resend.ts";
-import * as $$$$$$$$$$$3 from "./apps/site.ts";
+import * as $$$$$$$$$$$3 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$4 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/AboutUs.tsx";
 import * as $$$$$$1 from "./sections/Faq.tsx";
 import * as $$$$$$2 from "./sections/Footer.tsx";
@@ -40,7 +41,8 @@ const manifest = {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
     "site/apps/deco/htmx.ts": $$$$$$$$$$$1,
     "site/apps/deco/resend.ts": $$$$$$$$$$$2,
-    "site/apps/site.ts": $$$$$$$$$$$3,
+    "site/apps/decohub.ts": $$$$$$$$$$$3,
+    "site/apps/site.ts": $$$$$$$$$$$4,
   },
   "name": "site",
   "baseUrl": import.meta.url,
